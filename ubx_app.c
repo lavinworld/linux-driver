48d2d07049956e3f1169df86208c670f3b80d041
lavinworld <77840301+lavinworld@users.noreply.github.com>
2021-10-24 16:49:48 +0800

    Update ubx_app.c

--- ubx_app.c
+++ ubx_app.c
@@ -1,11 +1,47 @@
-/*
-navit-trunk github:
-https://github.com/navit-gps/navit
-*/
-
 #include "stdio.h"
 #include "stdlib.h"
 #include <string.h>
+
+static int get_cmd(void)
+{
+    int ret = 0;
+    FILE *fp;
+    char str[] = "0";
+
+    fp = fopen("./cmdlist.txt","r");
+    if(fp == NULL){
+        printf("open fail\r\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    return ret;
+}
+
+static int clear_para(void)
+{
+    int ret = 0;
+    FILE *fp;
+    char str[] = "0";
+
+    fp = fopen("./cmdlist.txt","r+");
+    if(fp == NULL){
+        printf("open fail\r\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek( fp, 4, SEEK_SET );
+    fwrite(str, strlen(str) , 1, fp );
+
+    fseek( fp, 16, SEEK_SET );
+    fwrite(str, strlen(str) , 1, fp );
+
+    fseek( fp, 28, SEEK_SET );
+    fwrite(str, strlen(str) , 1, fp );
+
+    return ret;
+}
 
 int main(void)
 {
@@ -15,6 +51,9 @@ int main(void)
     size_t len = 0;
     ssize_t read;
 
+    set_para();
+
+#if 0
     fp = fopen("./cmdlist.txt","r");
     if(fp == NULL){
         printf("open fail\r\n");
@@ -42,7 +81,9 @@ int main(void)
         line = NULL;
     }
 	exit(EXIT_SUCCESS);
-   
+#endif
+
+
 
     return ret;
 }
