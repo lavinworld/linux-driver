a75c7a47fb346986b08001767de2c9456f830fba
lavin <xlhcgd@163.com>
2022-07-27 22:18:10 +0800

    add other project

---
+++ 18_dtsplatform/.tmp_versions/leddriver.mod
@@ -0,0 +1,2 @@
+/home/lavin/work/git-src/imx6u/demo/imx6-linux-diver/18_dtsplatform/leddriver.ko
+/home/lavin/work/git-src/imx6u/demo/imx6-linux-diver/18_dtsplatform/leddriver.o
--- 1_chrdevbase/chrdevbaseApp.c
+++ 1_chrdevbase/chrdevbaseApp.c
@@ -5,18 +5,7 @@
 #include "fcntl.h"
 #include "stdlib.h"
 #include "string.h"
-/***************************************************************
-Copyright © ALIENTEK Co., Ltd. 1998-2029. All rights reserved.
-文件名		: chrdevbaseApp.c
-作者	  	: 左忠凯
-版本	   	: V1.0
-描述	   	: chrdevbase驱测试APP。
-其他	   	: 使用方法：./chrdevbase /dev/chrdevbase <1>|<2>
-  			 argv[2] 1:读文件
-  			 argv[2] 2:写文件		
-论坛 	   	: www.openedv.com
-日志	   	: 初版V1.0 2019/1/30 左忠凯创建
-***************************************************************/
+
 
 static char usrdata[] = {"usr data!"};
 
